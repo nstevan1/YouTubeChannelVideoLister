3a9177b46c461afbd3ebc9ea6992a0297dfc79b5
ninastevanovic <ninastevanovic@yahoo.com>
2024-10-15 00:55:39 -0700

    Added webView.setWebViewClient(new WebViewClient()); to MainActivity.java

--- app/src/main/java/com/example/youtube/MainActivity.java
+++ app/src/main/java/com/example/youtube/MainActivity.java
@@ -1,17 +1,16 @@
 package com.example.youtube;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.webkit.WebView;
-
+import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView webView = findViewById(R.id.webview);
-        webView.loadData("", "text/html", "utf-8");
+        webView.setWebViewClient(new WebViewClient());
+        String data = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"></head><body><section id=\"overlay\"><form action=\"https://www.google.com\" method=\"GET\"><h1>What is the channel id?</h1><section><input type=\"text\"></section><hr><section class=\"submission\"><form><input type=\"submit\" value=\"Submit\"></form></section></form></section></body></html>";
+        webView.loadData(data, "text/html", "utf-8");
     }
 }

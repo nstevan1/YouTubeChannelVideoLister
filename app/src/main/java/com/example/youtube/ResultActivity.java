b5a1d196496ce26f8878ff26e9b15e964589d7bd
nstevan1 <nstevan1@asu.edu>
2024-12-12 00:09:57 -0800

    Make my app load http://164.92.78.19/cgi-bin/

--- app/src/main/java/com/example/youtube/ResultActivity.java
+++ app/src/main/java/com/example/youtube/ResultActivity.java
@@ -1,6 +1,7 @@
 package com.example.youtube;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 public class ResultActivity extends AppCompatActivity {
     @Override
@@ -8,7 +9,10 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         String channelId = getIntent().getStringExtra("channelId");
-        TextView resultTextView = findViewById(R.id.resultTextView);
-        resultTextView.setText("Submitted channel id: " + channelId);
+        String url = "http://164.92.78.19/cgi-bin/YouTube_Fetch.py?channel_id=" + channelId;
+        WebView webView = findViewById(R.id.resultWebView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
     }
 }
